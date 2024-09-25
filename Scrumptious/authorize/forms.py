b087e014bf6def94d4676d350255df8432daee43
flurbudurbur <sebastianhoogendijk@gmail.com>
2024-09-25 13:42:00 +0200

    TailwindCSS forms complete

--- Scrumptious/authorize/forms.py
+++ Scrumptious/authorize/forms.py
@@ -1,25 +1,88 @@
-from http.cookiejar import is_HDN
-
 from django import forms
 from authorize.models import Users
+
 
 class UserLoginForm(forms.ModelForm):
     class Meta:
         model = Users
         fields = ['username', 'password']
-        widgets = {
-            'password': forms.PasswordInput()
+
+    username = forms.CharField(max_length=16,
+                               widget=forms.TextInput(
+                                   attrs={
+                                       'placeholder': 'Enter your username',
+                                       'class': 'p-2 border border-gray-300 rounded',
+                                   }
+                               ),
+                               label='Username',
+                               required=True,
+                               )
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'placeholder': 'Enter your password',
+            'class': 'p-2 border border-gray-300 rounded',
         }
+    ),
+        label='Password',
+        required=True,
+    )
+
+    # def __init__(self):
+    #     super(UserLoginForm, self).__init__()
+
 
 class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = Users
         fields = ['name', 'username', 'email', 'password']
 
+    name = forms.CharField(max_length=100,
+                           widget=forms.TextInput(
+                               attrs={
+                                   'placeholder': 'Enter your full name',
+                                   'class': 'p-2 border border-gray-300 rounded',
+                               }
+                           ),
+                           label='Full Name',
+                           required=True,
+                           )
+    username = forms.CharField(max_length=16,
+                               widget=forms.TextInput(
+                                   attrs={
+                                       'placeholder': 'Enter a username',
+                                       'class': 'p-2 border border-gray-300 rounded',
+                                   }
+                               ),
+                               label='Username',
+                               required=True,
+                               )
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={
+            'placeholder': 'Enter your email address',
+            'class': 'p-2 border border-gray-300 rounded',
+        }
+    ),
+        label='Email',
+        required=True,
+    )
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'placeholder': 'Enter a password',
+            'class': 'p-2 border border-gray-300 rounded',
+        }
+    ),
+        label='Password',
+        required=True,
+    )
+
     def __init__(self):
         super(UserRegistrationForm, self).__init__()
-        self.fields['name'] = forms.CharField(max_length=100, widget=forms.TextInput())
-        self.fields['username'] = forms.CharField(max_length=16, widget=forms.TextInput())
-        self.fields['email'] = forms.EmailField(widget=forms.EmailInput())
-        self.fields['password'] = forms.CharField(widget=forms.PasswordInput())
-        self.fields['confirm password'] = forms.CharField(widget=forms.PasswordInput())
+        self.fields['Confirm Password'] = forms.CharField(widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Confirm your password',
+                'class': 'p-2 border border-gray-300 rounded',
+            }
+        ),
+            label='Confirm Password',
+            required=True,
+        )
--- Scrumptious/authorize/views.py
+++ Scrumptious/authorize/views.py
@@ -19,6 +19,11 @@ def register_view(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            password = form.cleaned_data['password']
+            confirm_password = request.POST.get('confirm_password')
+            if password == confirm_password:
+                form.save()
+        else:
+            context['error'] = 'Passwords do not match.'
     context['form'] = form
     return render(request, 'register.html', context)
