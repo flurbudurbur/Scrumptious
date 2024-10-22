4ea664b99015b278d50cecc04fcd37643cf9f4a0
musta <mustafa.balci@hotmail.com>
2024-10-22 20:32:31 +0200

    Process of improving User sign in

--- Scrumptious/Scrumptious/models.py
+++ Scrumptious/Scrumptious/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Ingredients(models.Model):
-    name = models.CharField(max_length=50)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='scrumptious_ingredients')
+
+    def __str__(self):
+        return self.name
--- Scrumptious/posts/forms.py
+++ Scrumptious/posts/forms.py
@@ -1,16 +1,8 @@
 from django import forms
-
 from Scrumptious.models import Ingredients
 from .models import Post, Comments, Likes
 
-
 class MakePost(forms.ModelForm):
-    # ingredients = forms.ModelChoiceField(
-    #     queryset=Ingredients.objects.all().get('name'),
-    #     widget=forms.SelectMultiple(attrs={'class': 'p-2 border border-gray-300 rounded'}),
-    #     required=True
-    # )
-
     class Meta:
         model = Post
         fields = ['title', 'preparation', 'description', 'image']
@@ -26,16 +18,12 @@ class MakePost(forms.ModelForm):
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'p-2 border border-gray-300 rounded'
         self.fields['title'].widget.attrs['placeholder'] = 'Title'
-        # self.fields['ingredients'].widget.attrs['placeholder'] = 'Ingredients go here..'
-        # self.fields['ingredients'].widget.attrs['rows'] = 3
         self.fields['preparation'].widget.attrs['placeholder'] = 'Preparation goes here..'
         self.fields['preparation'].widget.attrs['rows'] = 3
         self.fields['description'].widget.attrs['placeholder'] = 'Description goes here...'
         self.fields['description'].widget.attrs['rows'] = 3
         self.fields['image'].widget.attrs['placeholder'] = 'Image'
-        self.fields['image'].widget.attrs['class'] = 'p-2 border border-gray-300 rounded'
         self.fields['image'].widget.attrs['accept'] = 'image/*'
-
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -52,8 +40,20 @@ class CommentForm(forms.ModelForm):
         self.fields['comment'].widget.attrs['placeholder'] = 'Comment'
         self.fields['comment'].widget.attrs['rows'] = 3
 
-
 class LikeForm(forms.ModelForm):
     class Meta:
         model = Likes
         fields = ['liked']
+
+class AddIngredient(forms.ModelForm):
+    class Meta:
+        model = Ingredients
+        fields = ['name']
+        labels = {
+            'name': 'Ingredient Name',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(AddIngredient, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['class'] = 'p-2 border border-gray-300 rounded'
+        self.fields['name'].widget.attrs['placeholder'] = 'Ingredient Name'
